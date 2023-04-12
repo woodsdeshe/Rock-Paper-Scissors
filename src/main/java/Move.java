e47ffac2c5b2ee679077e07d119fcf8e7cabab33
woodsdeshe <woodsdeshe94@gmail.com>
2023-04-12 18:56:28 -0400

    Player class and Move interface added

---
+++ src/main/java/Move.java
@@ -0,0 +1,3 @@
+public interface Move {
+    boolean beats(Move move);
+}
---
+++ src/main/java/Player.java
@@ -0,0 +1,7 @@
+import java.util.Optional;
+
+public abstract class Player {
+    private int score;
+    private boolean isWinner;
+    private Optional<Move> move;
+}
